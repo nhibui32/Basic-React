efc2da74fbd6cec6a668dc807bf296948799fada
Ngoc Anh Nhi Bui <148082202+nhibui32@users.noreply.github.com>
2024-09-12 00:15:34 -0500

    first commit

---
+++ src/components/MyComponent.js
@@ -0,0 +1,16 @@
+// class component 
+// function component 
+
+import React from 'react';
+
+function  MyComponent() {
+        return (
+            <div>
+                 my first component hello 
+                 {Math.random()}
+            </div>
+        );
+    }
+
+
+export default MyComponent;
